0533bb54fdca30dc431fdcb9233c76d1022f332b
netarnort <nogascarma1234@gmail.com>
2023-08-09 11:59:55 +0700

    page 54

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,14 @@ app.get('/hello/:person',function(req,res){
     res.send('say hello with ' +req.params.person)
 })
 
+app.get('user/:userId',function(req,res){
+    res.send('ดูข้อมูลผู้ใช้')
+})
+
+app.get('/users', function(req,res){
+    res.send('เรียกข้อมูล ผู้ใช้ทั้งหมด')
+})
+
 let port = 8081
 app.listen(port, function(){
     console.log('server running on ' + port)
