1d91071578c5b5ad1e12bda5b54c20bce28f8776
netarnort <nogascarma1234@gmail.com>
2023-08-16 09:54:35 +0700

    บทที่ 4

--- server/src/app.js
+++ server/src/app.js
@@ -1,36 +1,45 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/status',function(req,res){
-    res.send('Hello nodejs server belong to patipol kaeomuang')
+app.get('/status', function (req,res){
+    res.send('Hello nodejs server belong to nitiphumi')
 })
 
-app.get('/hello/:person',function(req,res){
-    console.log('hello - ' + req.params.person)
-    res.send('say hello with ' +req.params.person)
+app.get('/hello/:xxx', function(req,res){
+    console.log('hello - ' + req.params.xxx)
+    res.send('say hello with ' + req.params.xxx)
 })
 
-app.get('user/:userId',function(req,res){
-    res.send('ดูข้อมูลผู้ใช้'+ req.params.userId)
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน')
 })
 
-app.get('/users', function(req,res){
-    res.send('เรียกข้อมูล ผู้ใช้ทั้งหมด')
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
 })
 
-app.put('/user/:userId', function (req,res){
-    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userid + JSON.stringify(req.body.name))
+app.post('/user/', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body))
 })
 
-app.delete('/user/:userId', function (req,res){
-    res.send('ลบข้อมูลผู้ใช้ ' + req.params.userid + JSON.stringify(req.body.name))
+app.post('/user/', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+
+app.put('/user/:userId', function(req, res) {
+    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + JSON.stringify(req.body.name))
+})
+
+app.delete('/user/:userId', function(req, res) {
+    res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body))
 })
 
 let port = 8081
 app.listen(port, function(){
-    console.log('server running on ' + port)
+    console.log('server runing on ' + port)
 })
