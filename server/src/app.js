a43602869fa8c261e7f90f946b0c350149fd283f
netarnort <nogascarma1234@gmail.com>
2023-08-16 10:40:16 +0700

    บทที่ 5

--- server/src/app.js
+++ server/src/app.js
@@ -1,41 +1,43 @@
-let express = require('express')
-let bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to nitiphumi')
-})
+require('./route')(app)
 
-app.get('/hello/:xxx', function(req,res){
-    console.log('hello - ' + req.params.xxx)
-    res.send('say hello with ' + req.params.xxx)
-})
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server belonging to nitiphumi');
+});
+
+app.get('/hello/:name', function (req, res) {
+    console.log('hello - ' + req.params.name);
+    res.send('Say hello with ' + req.params.name);
+});
 
 app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
-})
+    res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
+});
 
 app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
-})
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด');
+});
 
-app.post('/user/', function (req, res) {
-    res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body))
-})
+app.post('/user/:userId', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body));
+});
 
-app.put('/user/:userId', function(req, res) {
-    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + JSON.stringify(req.body.name))
-})
+app.put('/user/:userId', function (req, res) {
+    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+});
 
-app.delete('/user/:userId', function(req, res) {
-    res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body))
-})
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body));
+});
 
-let port = 8081
-app.listen(port, function(){
-    console.log('server runing on ' + port)
-})
+const port = 8081;
+app.listen(port, function () {
+    console.log('Server running on port ' + port);
+});
