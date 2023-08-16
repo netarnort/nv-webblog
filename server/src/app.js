031cca0224bbf876fd5a99e19b4b08b602ccfc63
netarnort <nogascarma1234@gmail.com>
2023-08-16 09:52:53 +0700

    บทที่ 4

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,9 @@
 let express = require('express')
+let bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/status',function(req,res){
     res.send('Hello nodejs server belong to patipol kaeomuang')
@@ -11,11 +15,19 @@ app.get('/hello/:person',function(req,res){
 })
 
 app.get('user/:userId',function(req,res){
-    res.send('ดูข้อมูลผู้ใช้')
+    res.send('ดูข้อมูลผู้ใช้'+ req.params.userId)
 })
 
 app.get('/users', function(req,res){
     res.send('เรียกข้อมูล ผู้ใช้ทั้งหมด')
+})
+
+app.put('/user/:userId', function (req,res){
+    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userid + JSON.stringify(req.body.name))
+})
+
+app.delete('/user/:userId', function (req,res){
+    res.send('ลบข้อมูลผู้ใช้ ' + req.params.userid + JSON.stringify(req.body.name))
 })
 
 let port = 8081
