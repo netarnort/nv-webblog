e57da9db054b30da0caa12d6af1e37e73204cf6d
netarnort <nogascarma1234@gmail.com>
2023-09-13 11:11:13 +0700

    fix

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,31 +1,66 @@
+const { User } = require('../models')
+
 module.exports = {
 
-   
-    
-    // Get all users
-    index(req, res) {
-        res.send('ดูข้อมูลผู้ใช้งาน');
-    },
 
+
+    // Get all users
+    async index(req, res) {
+        try {
+            const users = await User.findAll()
+            res.send(users)
+        } catch (err) {
+            res.status(500).send({
+                error: 'The users information was incorrect'
+            })
+        }
+    },
     // Create user
-    create(req, res) {
-        res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body));
+    // res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body));
+    async create(req, res) {
+        try {
+            const user = await User.create(req.body)
+            res.send(user.toJSON())
+        } catch (err) {
+            res.status(500).send({
+                error: 'Create user incorrect'
+            })
+        }
     },
 
     // Edit user
-    put(req, res) {
-        res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+    // res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+    async put(req, res) {
+        try {
+            await User.update(req.body, {
+                where: {
+                    id: req.params.userId
+                }
+            })
+            res.send(req.body)
+        } catch (err) {
+            res.status(500).send({
+                error: 'Update user incorrect'
+            })
+        }
     },
 
     // Delete user
+    // res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body));
     delete(req, res) {
-        res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body));
     },
 
-     // Show user by id
-     show(req, res) {
-        res.send('ดูข้อมูลผู้ใช้งาน ' + req.params.userId + ' : ' + JSON.stringify(req.body));
-    },
+    // Show user by id
+    async show(req, res) {
+        try {
+            const user = await User.findByPk(req.params.userId)
+            res.send(user)
+        } catch (err) {
+            res.status(500).send({
+                error: 'The user information was incorrect'
+            })
+        }
+    }
+}
 
-};
 
