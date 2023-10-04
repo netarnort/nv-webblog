b17ae85f82405364c06fbfea685554fe382af7d2
netarnort <nogascarma1234@gmail.com>
2023-10-04 19:11:02 +0700

    page177

--- server/src/route.js
+++ server/src/route.js
@@ -1,10 +1,14 @@
+const UserAuthenController = require('./controllers/UserAuthenController')
 const UserController = require('./controllers/UserController')
+const isAuthenController = require('./authen/isAuthenController')
 
 module.exports = (app) =>{
 
 
     //get all user
-    app.get('/users', UserController.index)
+    app.get('/users', 
+    isAuthenController,
+    UserController.index)
 
     //create
     app.post('/user', UserController.create)
@@ -18,5 +22,6 @@ module.exports = (app) =>{
     //show by id
     app.get('/user/:userId', UserController.show)
 
+    app.post('/login', UserAuthenController.login)
 
 }
